3e2801d427579c88a3c371c4a6ef57fb0c60a30a
Robert Zuniga <github@mastertheday.com>
2019-07-17 04:57:00 -0400

    prepare to change

--- assets/javascript/javascript.js
+++ assets/javascript/javascript.js
@@ -5,112 +5,90 @@ console.log('TEST TEST TEST');
 //     $("#myStartButton").on("click", console.log("click works"));
 //     console.log("here");
 // };
-$('#submit').on("click", (event) => {
-    displayConsoleLog();
-    return false;
-}
-);
+// $('#submit').on("click", (event) => {
+//     displayConsoleLog();
+//     return false;
+// }
+// );
+
+$("#submit").click(displayConsoleLog);
+
+var myQuestionAnswer = [{
+        myQuestion: "Question 0",
+        mySelectionA: "Answer A0",
+        mySelectionB: "Answer B0",
+        mySelectionC: "Answer C0",
+        myAnswer: "mySelectionA"
+    },
+    {
+        myQuestion: "Question 1",
+        mySelectionA: "Answer A1",
+        mySelectionB: "Answer B1",
+        mySelectionC: "Answer C1",
+        myAnswer: "mySelectionB"
+    },
+    {
+        myQuestion: "Question 2",
+        mySelectionA: "Answer A2",
+        mySelectionB: "Answer B2",
+        mySelectionC: "Answer C2",
+        myAnswer: "mySelectionC"
+    }
+];
+
+// #questionsHere
+
 
 function displayConsoleLog() {
-   // event.preventDefault();
-    //event.stopPropagation();
-
-    var splashscreen;
-    var showQuestions;
-    var count = 0;
-
-
-
-
-    console.log("count ==> ", count)
-    console.log("click works")
-    
-
-    console.log("click works")
-    startMyTimer()
-    console.log("aferparty")
-} 
-
-  
-// var myQuestionAnswer = [{
-//         myQuestion: "Question 0",
-//         mySelectionA: "Answer A0",
-//         mySelectionB: "Answer B0",
-//         mySelectionC: "Answer C0",
-//         myAnswer: "mySelectionA"
-//     },
-//     {
-//         myQuestion: "Question 1",
-//         mySelectionA: "Answer A1",
-//         mySelectionB: "Answer B1",
-//         mySelectionC: "Answer C1",
-//         myAnswer: "mySelectionB"
-//     },
-//     {
-//         myQuestion: "Question 2",
-//         mySelectionA: "Answer A2",
-//         mySelectionB: "Answer B2",
-//         mySelectionC: "Answer C2",
-//         myAnswer: "mySelectionC"
-//     }
-// ];
-
-
-
-
-function startMyTimer() {
-    var secondTic = 5;
+    // event.preventDefault();
+     //event.stopPropagation();
+ 
+     var splashscreen;
+     var showQuestions;
+     var count = 5;
+ 
+     console.log("count ==> ", count)
+ 
+    var done = startMyTimer(5);
+ 
+     console.log("done =>", done)  
+ } 
+ 
+function startMyTimer(secondTic) {
+    var  mySecondTic = secondTic;
     var timeUP = false;
     var myTimer = setInterval(function () {
-            console.log("myTimer ==> ", secondTic);
-            $("#display-time").html("Time Remaining: " + secondTic +" s");
+            console.log("myTimer ==> ", mySecondTic);
+            $("#display-time").html("Time Remaining: " + mySecondTic +" s");
 
             if (timeUP === true) {
                 clearInterval(myTimer);
                 clearTimeout(myTimer);
-                $("#display-time").empty();
-            } else if (secondTic == 0) {
-                secondTic = 5;
+                $("#display-time").html("Time Remaining: " + mySecondTic +" s");
+                // $("#display-time").empty();
+            } else if (mySecondTic == 0) {
+                // secondTic = 5;
                 timeUP = true;
-                console.log("timeUp ==> ", timeUP);
             } else {
-                secondTic--;
+                mySecondTic--;
             }
+
+//do stuff here
+
+
+
+
+
+
+
+
+
+            console.log("secondTic => ", mySecondTic)
         }, 1000);
+
+
+
+
+    return timeUP;
 }
 
-
-// function stopMyTimer() {
-//     clearInterval(showQuestions);
-// }
-
-// function displaySplashScreen() {
-
-
-// }
-
-// displaySplashScreen();
-
-// $("#myStartButton").click(startMyTimer);
-
-
-
-// $(document).ready(function () {
-//     console.log("ready!");
-//     // let countDown = 5;
-//     // $("#timer").text(countDown);
-
-//     // var myVar = setInterval(myTimer, 1000);
-//     startMyTimer();
-
-//     // function myTimer() {
-//     //     if (countDown === 0) {
-//     //         console.log("We\'re done counting down!");
-//     //         var question1 = $('#question1 input:radio:checked').val()
-//     //         $("#timer").text(`Answer to Question 1: ${question1}`);
-//     //     } else {
-//     //         $("#timer").text(--countDown);
-//     //     }
-//     // }
-//     console.log("done");
-// });
--- assets/javascript/test-javascript.js
+++ assets/javascript/test-javascript.js
@@ -13,4 +13,55 @@ function displayConsoleLog(e) {
 
 } 
 
+var timeAllowed = 5;
+var done = startMyTimer(timeAllowed)
 
+
+     var splashscreen;
+     var showQuestions;
+     var count = 5;
+ 
+     console.log("count ==> ", count)
+ 
+    var done = startMyTimer(5);
+ 
+     console.log("done =>", done)  
+ 
+ 
+function startMyTimer(secondTic) {
+    var  mySecondTic = secondTic;
+    var timeUP = false;
+    var myTimer = setInterval(function () {
+            console.log("myTimer ==> ", mySecondTic);
+            $("#display-time").html("Time Remaining: " + mySecondTic +" s");
+
+            if (timeUP === true) {
+                clearInterval(myTimer);
+                clearTimeout(myTimer);
+                $("#display-time").html("Time Remaining: " + mySecondTic +" s");
+                // $("#display-time").empty();
+            } else if (mySecondTic == 0) {
+                // secondTic = 5;
+                timeUP = true;
+            } else {
+                mySecondTic--;
+            }
+
+//do stuff here
+
+
+
+
+
+
+
+
+
+            console.log("secondTic => ", mySecondTic)
+        }, 1000);
+
+
+
+
+    return timeUP;
+}
