6eeffce6477ee24ee0af2e0a9e401e3a9e71ac26
Robert Zuniga <github@mastertheday.com>
2019-07-31 02:12:38 -0400

    working trivia game

--- assets/javascript/javascript.js
+++ assets/javascript/javascript.js
@@ -1,137 +1,14 @@
 // Robert Zuniga Trivia javascript
 
+//Intro Page 
 $(document).ready(function () {
-    // start the game when user clicks on Start button
-    $("#start-button").on("click", gameState.startTimer);
+    $("#start-button").on("click", myTimer.startTimer);
 });
-
-// information about the state of game play
-
-var gameState = {
-    // set the time at 60 seconds, and count down by 1 second
-    timeRemaining: 10,
-
-    // start the timer, hide the start page, show the questions
-
-    startTimer: function () {
-        $("#timer").text("Time remaining: " + gameState.timeRemaining);
-        setInterval(gameState.countdown, 1000);
-        $("#start-page").hide();
-        displayQuestions();
-    },
-
-    // decrement the timer and update the UI; stop the timer at 0
-
-    countdown: function () {
-
-        gameState.timeRemaining--;
-        $("#timer").text("Time remaining: " + gameState.timeRemaining);
-
-        if (gameState.timeRemaining === 0) {
-            gameState.stopTimer();
-            $("#timer").empty();
-        }
-    },
-
-    // stop the timer and check the answers
-
-    stopTimer: function () {
-       clearInterval();
-       checkAnswers();
-    },
-
-    // hide the quetions and display the end page with results
-
-    showEndPage: function (numCorrect, numIncorrect, numUnanswered) {
-        $("#end-page").show();
-        $("#questions-box").empty();
-        $("#timer").empty();
-        $("#timer").hide();
-
-        $("#correct-answers").text("Correct: " + numCorrect);
-        $("#incorrect-answers").text("Incorrect: " + numIncorrect);
-        $("#unanswered").text("Not Answered: " + numUnanswered);
-    }
-}
-
-// functions to handle the building questions page and scoring
-
-
-
-    // pull questions from the array of questions, loop through them, and append to UI
-
-    function displayQuestions() {
-
-        var divContainer = $("#questions-box");
-
-        // var answerGroup = $(".form-check");
-
-        divContainer.append('<h2>Answer the following questions:</h2>');
-
-        for (var i = 0; i < questionVault.length; i++) {
-
-            divContainer.append('<div id="question">' + questionVault[i].question + '</div>');
-
-            var answer1 = questionVault[i].answers[0];
-
-            var answer2 = questionVault[i].answers[1];
-
-            var answer3 = questionVault[i].answers[2];
-
-            divContainer.append('<div class="form-check"><input class="form-check-input" type="radio" name="radio-group' + i + '" id="radio' + i + '"><label class="form-check-label" id="radio' + i + 'label" for="radio' + i + '">' + answer1 + '</label></div>');
-
-            divContainer.append('<div class="form-check"><input class="form-check-input" type="radio" name="radio-group' + i + '" id="radio' + i + '"><label class="form-check-label" id="radio' + i + 'label" for="radio' + i + '">' + answer2 + '</label></div>');
-
-            divContainer.append('<div class="form-check"><input class="form-check-input" type="radio" name="radio-group' + i + '" id="radio' + i + '"><label class="form-check-label" id="radio' + i + 'label" for="radio' + i + '">' + answer3 + '</label></div>');
-        }
-
-        // add a Done button to the end of the page and register its click handler
-
-        // var doneButton = '<button class="btn btn-primary" id="done-button" type="submit">Done</button>';
-
-        // divContainer.append(doneButton);
-
-        // $("#done-button").on("click", gameState.stopTimer);
-    }
-
-    // test if the user answers are correct, incorrect, or if there are unanswered questions
-
-    function checkAnswers() {
-
-        var correctAnswer;
-        var userAnswer;
-        var numCorrect = 0;
-        var numIncorrect = 0;
-        var numUnanswered = 0;
-
-        // loop through to compare the text of the label with the user answer
-        // increment score counts appropriately
-
-        for (var i = 0; i < questionVault.length; i++) {
-
-            correctAnswer = questionVault[i].correct;
-            userAnswer = $('input[id=radio' + i + ']:checked + label').text();
-
-            if (userAnswer === correctAnswer) {
-                numCorrect++;
-            } else if (userAnswer === "") {
-                numUnanswered++;
-            } else if (userAnswer !== correctAnswer) {
-                {
-                    numIncorrect++;
-                }
-            }
-        }
-        // show the end page with the score tally
-        gameState.showEndPage(numCorrect, numIncorrect, numUnanswered);
-    }
-
-// array of objects with the questions, possible answers, and the correct answer
-
+////////////////////////////////////////////////////////////////
+//questions available
+////////////////////////////////////////////////////////////////
 var questionVault =
-
-    [
-        {
+    [{
             question: "Which one is NOT a primary color?",
             answers: ["red", "orange", "blue"],
             correct: "orange"
@@ -142,9 +19,79 @@ var questionVault =
             correct: "plasma"
         },
         {
-            question: "What is the fastest spped an object can have?",
+            question: "What is the fastest speed an object can have?",
             answers: ["speed of light", "speed of sound", "speed of plasma"],
-            correct: "speed of plasma"
+            correct: "speed of light"
         },
-        
     ]
+////////////////////////////////////////////////////////////////
+//post questions
+////////////////////////////////////////////////////////////////
+function displayQuestions() {
+    var divContainer = $("#questions-box");
+    divContainer.append('<h2>Answer the following questions:</h2>');
+    for (var i = 0; i < questionVault.length; i++) {
+        divContainer.append('<div id="question">' + questionVault[i].question + '</div>');
+        var answer1 = questionVault[i].answers[0];
+        var answer2 = questionVault[i].answers[1];
+        var answer3 = questionVault[i].answers[2];
+        divContainer.append('<div class="form-check"><input class="form-check-input" type="radio" name="radio-group' + i + '" id="radio' + i + '"><label class="form-check-label" id="radio' + i + 'label" for="radio' + i + '">' + answer1 + '</label></div>');
+        divContainer.append('<div class="form-check"><input class="form-check-input" type="radio" name="radio-group' + i + '" id="radio' + i + '"><label class="form-check-label" id="radio' + i + 'label" for="radio' + i + '">' + answer2 + '</label></div>');
+        divContainer.append('<div class="form-check"><input class="form-check-input" type="radio" name="radio-group' + i + '" id="radio' + i + '"><label class="form-check-label" id="radio' + i + 'label" for="radio' + i + '">' + answer3 + '</label></div>');
+    }
+}
+////////////////////////////////////////////////////////////////
+// run the timer
+////////////////////////////////////////////////////////////////
+var myTimer = {
+    timeRemaining: 10, //10 seconds
+    startTimer: function () {
+        $("#timer").text("Time remaining: " + myTimer.timeRemaining);
+        setInterval(myTimer.countdown, 1000);
+        $("#start-page").hide();
+        displayQuestions();
+    },
+    countdown: function () {
+        myTimer.timeRemaining--;
+        $("#timer").text("Time remaining: " + myTimer.timeRemaining);
+        if (myTimer.timeRemaining === 0) {
+            myTimer.stopTimer();
+            $("#timer").empty();
+        }
+    },
+    stopTimer: function () {
+        clearInterval();
+        checkAnswers();
+    }
+}
+////////////////////////////////////////////////////////////////
+//check answers submitted and post results
+////////////////////////////////////////////////////////////////
+function checkAnswers() {
+    var correctAnswer;
+    var userAnswer;
+    var numCorrect = 0;
+    var numIncorrect = 0;
+    var numUnanswered = 0;
+
+    for (var i = 0; i < questionVault.length; i++) {
+        correctAnswer = questionVault[i].correct;
+        userAnswer = $('input[id=radio' + i + ']:checked + label').text();
+        if (userAnswer === correctAnswer) {
+            numCorrect++;
+        } else if (userAnswer === "") {
+            numUnanswered++;
+        } else if (userAnswer !== correctAnswer) {
+            {
+                numIncorrect++;
+            }
+        }
+    }
+        $("#end-page").show();
+        $("#questions-box").empty();
+        $("#timer").empty();
+        $("#timer").hide();
+        $("#correct-answers").text("Correct: " + numCorrect);
+        $("#incorrect-answers").text("Incorrect: " + numIncorrect);
+        $("#unanswered").text("Not Answered: " + numUnanswered);
+}
